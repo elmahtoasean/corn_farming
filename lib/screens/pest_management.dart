6692f56f612f988a478670b1f774a20af633505e
elmahtoasean <toasean.csecu@gmail.com>
2025-09-23 23:47:53 +0600

    Ititial commit

--- lib/screens/pest_management.dart
+++ lib/screens/pest_management.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class PestManagement extends StatelessWidget {
+class PestManagement extends StatefulWidget {
   const PestManagement({super.key});
 
   @override
+  State<PestManagement> createState() => _PestManagementState();
+}
+
+class _PestManagementState extends State<PestManagement> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Extract the video ID from the YouTube Shorts link
+    const videoUrl = 'https://www.youtube.com/shorts/tdwUMyJo_CQ?si=ARlUjNFB0KmwOTx9';
+    final videoId = YoutubePlayer.convertUrlToId(videoUrl);
+
+    _controller = YoutubePlayerController(
+      initialVideoId: videoId ?? '',
+      flags: const YoutubePlayerFlags(
+        autoPlay: false,
+        mute: false,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pest Management')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          elevation: 6,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: YoutubePlayer(
+              controller: _controller,
+              showVideoProgressIndicator: true,
+              progressIndicatorColor: Colors.red,
+              progressColors: const ProgressBarColors(
+                playedColor: Colors.red,
+                handleColor: Colors.redAccent,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
